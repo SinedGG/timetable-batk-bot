1163a3b071c5eba6263eb885c224324df3dd4f7c
Sined <denis2320032@gmail.com>
2021-10-06 22:54:24 +0300

    Add /getlog 🌠

--- index.js
+++ index.js
@@ -134,6 +134,21 @@ bot.command("version", (ctx) => {
   ctx.reply("Vesion: " + cfg.version);
 });
 
+bot.command("getlog", (ctx) => {
+  if(ctx.message.chat.id == "460266962"){
+  bot.telegram.sendDocument(
+      ctx.message.chat.id,
+      { source: "./log/last.txt" },
+      {disable_notification: true,
+       caption: "Last log" }
+    ).catch(err =>{
+      if(err.message == '400: Bad Request: file must be non-empty'){
+          ctx.reply("Log file is empty!"); 
+      }
+    })
+  }
+});
+
 bot.command("stats", (ctx) => {
   db.query("SELECT chat_id FROM users", function (err, result) {
     var user = result;
