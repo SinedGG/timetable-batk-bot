b3df7a0d394900f718d6707953fd6a1b226f9998
Sined <denis2320032@gmail.com>
2021-10-28 11:00:33 +0300

    Maybe fix 🤷‍♂️

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ var schedule = require("node-schedule");
 var request = require("request");
 
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
+  connectionLimit: 10,
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   database: process.env.DB_NAME,
