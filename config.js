f05b9f7d198cb58199a04aa52f7356c04f524d31
SinedGG <denis2320032@gmail.com>
2022-08-23 18:09:07 +0300

    ⬆️ | Updated config

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  url: "http://localhost/pdf/zm.pdf",
+  url: "https://www.batk.nubip.edu.ua/images/zm.pdf",
   start_date: "11/15/2020",
   adminchatid: [1545347454, 460266962],
   pdfpatch: "./file/zm.pdf",
