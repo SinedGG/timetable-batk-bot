f99a6c04af0fd34a77019fdbc084e438d9d8994b
Sined <denis2320032@gmail.com>
2023-02-05 20:45:43 +0200

    Changed url

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  url: "https://www.batk.nubip.edu.ua/images/zm.pdf",
+  url: "https://www.bati.nubip.edu.ua/images/EDU_ROZ_INS/Zm_Roz_in.pdf",
   start_date: "02/9/2022",
   adminchatid: [1545347454, 460266962],
   pdfpatch: "./file/zm.pdf",
