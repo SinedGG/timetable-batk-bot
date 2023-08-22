bc8393b7cf0f10cbe5c5a497d6c6984588fa246a
Denis <51417134+SinedGG@users.noreply.github.com>
2023-08-23 02:14:47 +0300

    🍃 | Add missing catch

--- modules/dailyMailing.js
+++ modules/dailyMailing.js
@@ -5,13 +5,17 @@ module.exports = (bot, db) => {
     var [users] = await db.query(`SELECT * FROM users `);
     if (users.length == 0) return;
     for (var i = 0; i < users.length; i++) {
-      await bot.telegram.sendPhoto(
-        users[i].chat_id,
-        { source: "./file/silence.jpg" },
-        {
-          disable_notification: true,
-        }
-      );
+      await bot.telegram
+        .sendPhoto(
+          users[i].chat_id,
+          { source: "./file/silence.jpg" },
+          {
+            disable_notification: true,
+          }
+        )
+        .catch((err) => {
+          console.log(err);
+        });
       console.log(
         `[dailyMailing] Спроба надсилання розсилки для - ${users[i].chat_id}`
       );
