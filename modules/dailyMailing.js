f2424b3f8b8d56a2807ccc9fa55661352b0954b9
SinedGG <denis2320032@gmail.com>
2023-08-30 00:26:39 +0300

    🍃 | Create dailyMailing module

---
+++ modules/dailyMailing.js
@@ -0,0 +1,26 @@
+module.exports = (bot) => {
+  const schedule = require("node-schedule");
+
+  schedule.scheduleJob("0 58 5 * * *", async () => {
+    const users = await require("../models/user").getAll();
+    if (users.length == 0) return;
+    for (var i = 0; i < users.length; i++) {
+      await bot.telegram
+        .sendPhoto(
+          users[i].tg_id.toString(),
+          { source: "./temp/silence.jpg" },
+          {
+            disable_notification: true,
+          }
+        )
+        .catch((err) => {
+          console.log(err);
+        });
+      console.log(
+        `[dailyMailing] Спроба надсилання розсилки для - ${users[
+          i
+        ].tg_id.toString()}`
+      );
+    }
+  });
+};
