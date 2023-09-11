0428b5a7336b56ad75d14f18de7e0fdc84c857b6
SinedGG <denis2320032@gmail.com>
2023-09-11 18:30:35 +0300

    🍂 | Refreshed config

--- batk-bot.config.js
+++ batk-bot.config.js
@@ -1,13 +1,10 @@
 module.exports = {
   apps: [
     {
-      name: "timetable-batk-bot",
-      cwd: "/home/ubuntu/pm/timetable-batk-bot",
+      name: "batk-bot",
+      cwd: "/home/ubuntu/pm/batk-bot",
       script: "index.js",
-      watch: true,
-      watch_delay: 5000,
       log_date_format: "YYYY-MM-DD HH:mm Z",
-      ignore_watch: ["temp"],
 
       env: {
         TG_TOKEN: "",
